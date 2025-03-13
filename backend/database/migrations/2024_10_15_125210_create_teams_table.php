452eed9bb5c14948c8a9f46822972cd41bc58e51
Justin <manglerjustin2@gmail.com>
2025-03-13 06:26:21 -0400

    test

--- backend/database/migrations/2024_10_15_125210_create_teams_table.php
+++ backend/database/migrations/2024_10_15_125210_create_teams_table.php
@@ -10,16 +10,16 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->string('tag1', 2048)->nullable();
-            $table->string('tag2', 2048)->nullable();
-            $table->string('tag3', 2048)->nullable();
-            $table->string('tag4', 2048)->nullable();
-            $table->string('tag5', 2048)->nullable();
-            $table->string('tag6', 2048)->nullable();
-            $table->string('tag7', 2048)->nullable();
-            $table->string('tag8', 2048)->nullable();
-            $table->string('competencies', 2048)->nullable(); // Use json type for competencies
-            $table->string('links', 2048)->nullable();
+            $table->string('tag1', 511)->nullable();
+            $table->string('tag2', 511)->nullable();
+            $table->string('tag3', 511)->nullable();
+            $table->string('tag4', 511)->nullable();
+            $table->string('tag5', 511)->nullable();
+            $table->string('tag6', 511)->nullable();
+            $table->string('tag7', 511)->nullable();
+            $table->string('tag8', 511)->nullable();
+            $table->string('competencies', 511)->nullable(); // Use json type for competencies
+            $table->string('links', 511)->nullable();
             $table->timestamps();
         });
     }
